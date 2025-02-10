e9246282a03cb5163c45dcd0478cad718eb8efe3
Jesús Abet <j_abet@me.com>
2025-02-10 17:21:11 +0100

    Añadidas pequeñas modificaciones

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ target_date = datetime(2025, 11, 25, 10, 5, 00)
 root = tk.Tk()
 root.title("Days to go to Japan!")
 
+# Eliminar los bordes de la ventana
+root.overrideredirect(True)
+
+
 size_x = 200
-size_y = 100
+size_y = 120
 particle_size_x = 5
 particle_size_y = 5
 
@@ -40,6 +44,16 @@ leaf_photo = ImageTk.PhotoImage(leaf_image)
 countdown_label = tk.Label(root, font=("Helvetica", 10))
 countdown_label.pack()
 
+text = [
+    "¡おはよう Japón!", 
+    "Ya queda menos", 
+    "¿Tienes todo listo?",
+    "No te olvides de la maleta",
+    "¿Tienes listo el pasaporte?"
+]
+
+
+
 # Lista para almacenar las partículas
 particles = []
 
@@ -55,7 +69,7 @@ def create_particle():
 def update_particles():
     for particle in particles:
         canvas.move(particle, 0, 1)
-        if canvas.coords(particle)[1] > (size_y - particle_size_y+3):
+        if canvas.coords(particle)[1] > size_y - particle_size_y // 2:
             canvas.delete(particle)
             particles.remove(particle)
     root.after(50, update_particles)
@@ -70,8 +84,45 @@ def update_countdown():
     countdown_label.config(text=f"{days} días, {hours} horas,  {minutos} minutos")
     root.after(1000, update_countdown)
 
+def update_text():
+    canvas.delete("text")
+    selected_text = random.choice(text)
+    x = size_x // 2
+    y = size_y // 2
+    halo_color = "white"
+    text_color = "red"
+    font = ("Helvetica", 12)
+    
+    halo_items = []
+    # Crear el halo
+    offsets = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for dx, dy in offsets:
+        halo_item = canvas.create_text(x + dx, y + dy, text=selected_text, font=font, fill=halo_color, tag="text", width=size_x - 10)
+        halo_items.append(halo_item)
+    # Crear el texto principal
+    text_item = canvas.create_text(x, y, text=selected_text, font=font, fill=text_color, tag="text", width=size_x - 10)
+    
+    # Iniciar el fadeout después de 5 segundos
+    root.after(5000, fadeout_text, halo_items + [text_item])
+    
+
+    root.after(60000, update_text)
+
+# Función para realizar el fadeout del texto
+def fadeout_text(items, alpha=1.0, step=0.10):
+    if alpha > 0:
+        color = f"#{int(255 * alpha):02x}{int(255 * alpha):02x}{int(255 * alpha):02x}"
+        for item in items:
+            canvas.itemconfig(item, fill=color)
+        root.after(100, fadeout_text, items, alpha - step)
+    else:
+        for item in items:
+            canvas.delete(item)
+
+
 # Iniciar la creación y actualización de partículas
 update_countdown()
+update_text()
 create_particle()
 update_particles()
 
